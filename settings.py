e1f38a99ef3d8bdf8fcc79303ae4ce26d81aae0f
Karlo Melendez-Pena <mgkarlo@gmail.com>
2022-08-20 13:23:29 -0400

    Add player settings for complex movement and energy mechanics.

--- settings.py
+++ settings.py
@@ -9,5 +9,12 @@ class Settings:
         self.ui_color = (240, 248, 255)
 
         # Player
-        self.x_speed = 0.9
-        self.y_speed = 1.2
+        self.player_x_speed = 0.9
+        self.player_y_speed = 1.2
+        self.player_fwd_drain = 0.5
+        self.player_rwd_drain = 3.5
+        self.player_lat_drain = 1.5
+        self.player_boost_drain = 4.5
+        self.player_energy_regen = 5
+        self.player_boost_mult = 2
+        self.player_drain_mult = 0.1
